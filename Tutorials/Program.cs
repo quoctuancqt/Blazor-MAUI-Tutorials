b4a4400e4e32cdb4a6079bd126ec040b016947ee
Tuan Cao <tuan.cao@successsoftware.global>
2023-01-03 11:08:18 +0700

    Implement Authentication and Authorization

--- Tutorials/Program.cs
+++ Tutorials/Program.cs
@@ -1,4 +1,5 @@
 using BlazorTutorial;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<ExternalAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<ExternalAuthStateProvider>());
 
 await builder.Build().RunAsync();
