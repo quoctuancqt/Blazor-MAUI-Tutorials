b6b83dfd56288f6c79aadc430c6f89074ba872b4
Tuan Cao <tuan.cao@successsoftware.global>
2023-02-10 16:41:04 +0700

    update code

---
+++ ToDoApi/Program.cs
@@ -0,0 +1,109 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+
+    var scheme = new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Flows = new OpenApiOAuthFlows
+        {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri("https://demo.duendesoftware.com/connect/authorize"),
+                TokenUrl = new Uri("https://demo.duendesoftware.com/connect/token")
+            }
+        },
+        Type = SecuritySchemeType.OAuth2
+    };
+
+    options.AddSecurityDefinition("OAuth", scheme);
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Id = "OAuth", Type = ReferenceType.SecurityScheme }
+            },
+            new List<string> { }
+        }
+    });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                // base-address of your identityserver
+                options.Authority = "https://demo.duendesoftware.com";
+
+                // audience is optional, make sure you read the following paragraphs
+                // to understand your options
+                options.TokenValidationParameters.ValidateAudience = false;
+
+                // it's recommended to check the type header to avoid "JWT confusion" attacks
+                options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+            });
+// reference tokens
+//.AddOAuth2Introspection("introspection", options =>
+//{
+//    options.Authority = Constants.Authority;
+
+//    options.ClientId = "resource1";
+//    options.ClientSecret = "secret";
+//});
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.OAuthClientId("interactive.public");
+        options.OAuthScopes("profile", "openid", "api", "email", "offline_access");
+        options.OAuthUsePkce();
+    });
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi()
+.RequireAuthorization();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
